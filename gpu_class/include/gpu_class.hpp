2e7ef97ef856842a1c0cc19f28ecc79b159f1110
Pierre Nicolay <nicolaypierre.95@gmail.com>
2020-10-24 18:14:16 +0100

    example of class implementation on a GPU device

---
+++ gpu_class/include/gpu_class.hpp
@@ -0,0 +1,36 @@
+#ifndef __CUDA_CLASS_H__
+#define __CUDA_CLASS_H__
+
+#define STEPS 100000
+
+class ModelGpu{
+public:
+  __host__ __device__ void advance();
+  __host__ __device__ void setX(int x);
+  __host__ __device__ void setN(int n);
+  __host__ __device__ int getX();
+  __host__ __device__ int getN();
+  __host__ __device__ ModelGpu();
+  __host__ __device__ void init(int x);
+
+private:
+  int x_;
+  int n_;
+};
+
+class Model{
+public:
+  Model(int n);
+  ~Model();
+  void sim();
+  void memcpy_set_data(int* x);
+  void memcpy_get_data(int* x);
+private:
+  int n_;
+  size_t bytes_;
+  int* d_x;
+  ModelGpu* d_models;
+
+};
+
+#endif
