0f45578f0731eb7bd1d1aa583b96df1c7788a01f
Pierre Nicolay <nicolaypierre.95@gmail.com>
2020-10-28 12:49:37 +0000

    moved test code into separate function

--- mppi_gpu/src/main.cu
+++ mppi_gpu/src/main.cu
@@ -22,10 +22,80 @@
  * on the host device.
  */
 
-void to_csv(float* x, size_t size, size_t sample){
+bool test_sim_gpu(float* data,
+              size_t samples,
+              size_t state_dim,
+              size_t act_dim,
+              float dt,
+              float act,
+              float init){
+
+  std::chrono::time_point<std::chrono::system_clock> t1;
+  std::chrono::time_point<std::chrono::system_clock> t2;
+  std::chrono::duration<double, std::milli> fp_ms;
+  double delta;
+
+  PointMassModelGpu* models = new PointMassModelGpu[samples];
+  float** x;
+  float* u;
+  float x_gain[state_dim];
+  float u_gain[act_dim];
+
+  u_gain[0] = dt*dt/2.0;
+  u_gain[1] = dt;
+  x_gain[0] = 1;
+  x_gain[1] = dt;
+  x_gain[2] = 0;
+  x_gain[3] = 1;
+
+  x = (float**) malloc(sizeof(float*)*samples);
+  u = (float*) malloc(sizeof(float)*STEPS*act_dim);
+
+  for(int i=0; i < STEPS; i++){
+    u[i*act_dim + 0] = act;
+    u[i*act_dim + 1] = act;
+  }
+
+  for(int i=0; i < samples; i++){
+    x[i] = (float*) malloc(sizeof(float)*STEPS*state_dim);
+    x[i][0] = init;
+    x[i][1] = init;
+    x[i][2] = init;
+    x[i][3] = init;
+    models[i].init(x[i], 0, u, x_gain, state_dim, u_gain, act_dim);
+  }
+
+  t1 = std::chrono::system_clock::now();
+
+  // run the same code for on the cpu to evaluate the improvement.
+  for(int i=0; i < samples; i++){
+    models[i].run();
+  }
+
+  t2 = std::chrono::system_clock::now();
+  fp_ms = t2 - t1;
+  delta = fp_ms.count();
+
+  std::cout << "Sequencial execution time: " << delta << "ms" << std::endl;
+
+
+  // free the memory.
+  for(int i=0; i<samples; i++){
+    for (int j=0; j<STEPS; j++){
+      for (int k=0; k<state_dim; k++){
+        if (!fabs(data[i*STEPS*state_dim + j*state_dim + k] - x[i][j*state_dim + k]) < TOL ){
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+void to_csv(std::string filename, float* x, size_t size, size_t sample){
+  std::cout << "Saving data to file...: " << std::flush;
   std::ofstream outfile;
   // create a name for the file output
-  std::string filename = "exampleOutput.csv";
 
   outfile.open(filename);
 
@@ -36,6 +106,8 @@ void to_csv(float* x, size_t size, size_t sample){
     }
   }
   outfile.close();
+  std::cout << "Done" << std::endl;
+  return;
 }
 
 int main(){
@@ -49,20 +121,11 @@ int main(){
   int act_dim = 2;
   int state_dim = 4;
 
-  int n = 20000;
+  int n = 10000;
 
   float dt = 1.;
 
-  /*
-   * copy of our models on host. Should ultimatly
-   * be removed and the models object should be stored on
-   * device only.
-   */
-  PointMassModelGpu* models = new PointMassModelGpu[n];
-  /*
-   * Model Gpu wrapper, this will allow to offer one entry
-   * interface to the controller that is not device or host specific.
-   */
+
   PointMassModel model = PointMassModel(n, STEPS, dt);
   /*
    * The state data stored on host. In this example,
@@ -70,19 +133,14 @@ int main(){
    * array with all the following states. Thus we need a int[n]
    * array to the input data.
    */
-
   float* h_x;
   float* h_u;
-
   /*
    * This variable stores the output result. In this
    * example it is a n*steps array but it will be continous
    * in device memory. so a int* array will be easier to work with.
    */
-
   float* h_o;
-
-
   // allocate and init and res data.
   h_x = (float*) malloc(sizeof(float)*n*state_dim);
   h_u = (float*) malloc(sizeof(float)*n*STEPS*act_dim);
@@ -115,65 +173,15 @@ int main(){
 
   // get the data from the device.
   model.memcpy_get_data(h_o);
-  //std::cout << "Saving data to file...: ";
-  //to_csv(h_o, STEPS, n);
-  //std::cout << "Done" << std::endl;
-
-  {
-    float** x;
-    float* u;
-    float x_gain[state_dim];
-    float u_gain[act_dim];
-
-    u_gain[0] = dt*dt/2.0;
-    u_gain[1] = dt;
-    x_gain[0] = 1;
-    x_gain[1] = dt;
-    x_gain[2] = 0;
-    x_gain[3] = 1;
-
-    x = (float**) malloc(sizeof(float*)*n);
-    u = (float*) malloc(sizeof(float)*STEPS*act_dim);
-    for(int i=0; i < STEPS; i++){
-      u[i*act_dim + 0] = 0.01;
-      u[i*act_dim + 1] = 0.01;
-    }
-
-    for(int i=0; i < n; i++){
-      x[i] = (float*) malloc(sizeof(float)*STEPS*state_dim);
-      x[i][0] = 0.0;
-      x[i][1] = 0.0;
-      x[i][2] = 0.0;
-      x[i][3] = 0.0;
-      models[i].init(x[i], 0, u, x_gain, state_dim, u_gain, act_dim);
-    }
-
-    t1 = std::chrono::system_clock::now();
-
-    // run the same code for on the cpu to evaluate the improvement.
-    for(int i=0; i < n; i++){
-      models[i].run();
-    }
-
-    t2 = std::chrono::system_clock::now();
-    fp_ms = t2 - t1;
-    delta = fp_ms.count();
-
-    std::cout << "Sequencial execution time: " << delta << "ms" << std::endl;
 
 
-    // free the memory.
-    for(int i=0; i<n; i++){
-      for (int j=0; j<STEPS; j++){
-        for (int k=0; k<state_dim; k++){
-          assert(fabs(h_o[i*STEPS*state_dim + j*state_dim + k] - x[i][j*state_dim + k]) < TOL );
-        }
-      }
-    }
-    std::cout << "Test passed" << std::endl;
+  //to_csv(filename, h_o, STEPS, n);
+
+  if(test_sim_gpu(h_o, n, state_dim, act_dim, dt, 0.01, 0.0)){
+    std::cout << "Test passed!" << std::endl;
   }
 
-  std::cout << "Freeing memory... : ";
+  std::cout << "Freeing memory... : " << std::flush;
   free(h_x);
   free(h_o);
   free(h_u);
