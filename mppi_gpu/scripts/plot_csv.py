c2c82e22b1b60358bc34bfc41cd25643c08492a8
Pierre Nicolay <nicolaypierre.95@gmail.com>
2020-12-01 12:18:36 +0000

    plotting scripts

--- mppi_gpu/scripts/plot_csv.py
+++ mppi_gpu/scripts/plot_csv.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import csv
 
 def getMetaData(csvfile):
@@ -13,7 +14,7 @@ def getMetaData(csvfile):
             id_prev = int(row['sample'])
         size += 1
     size /= sample
-    return int(sample), int(size-1)
+    return int(sample), int(size)
 
 def plot_file(file):
     x = []
@@ -34,16 +35,16 @@ def plot_file(file):
 
     with open(file) as csvfile:
         samples, size = getMetaData(csvfile)
-        x = np.zeros((samples, size+1))
-        y = np.zeros((samples, size+1))
-        vx = np.zeros((samples, size+1))
-        vy = np.zeros((samples, size+1))
-        ex = np.zeros((samples, size))
-        ey = np.zeros((samples, size))
-        ux = np.zeros((size))
-        uy = np.zeros((size))
-        ux_prev = np.zeros((size))
-        uy_prev = np.zeros((size))
+        x = np.zeros((samples, size))
+        y = np.zeros((samples, size))
+        vx = np.zeros((samples, size))
+        vy = np.zeros((samples, size))
+        ex = np.zeros((samples, size-1))
+        ey = np.zeros((samples, size-1))
+        ux = np.zeros((size-1))
+        uy = np.zeros((size-1))
+        ux_prev = np.zeros((size-1))
+        uy_prev = np.zeros((size-1))
         w = np.zeros((samples))
         c = np.zeros((samples))
         cost = np.zeros((samples))
@@ -59,10 +60,10 @@ def plot_file(file):
             y[id, i] = float(row['y'])
             vx[id, i] = float(row['x_dot'])
             vy[id, i] = float(row['y_dot'])
-            if i < size :
+            if i < size-1 :
                 ex[id, i] = float(row['e_x'])
                 ey[id, i] = float(row['e_y'])
-            if id < 1 and i < size:
+            if id < 1 and i < size-1:
                 ux[i] = float(row['u[0]'])
                 uy[i] = float(row['u[1]'])
                 ux_prev[i] = float(row['u_prev[0]'])
@@ -71,33 +72,37 @@ def plot_file(file):
                 w[id*size + i] = float(row['w'])
                 c[id*size + i] = float(row['c'])
             i += 1
-            i %= (size + 1)
+            i %= size
 
     for id, sample in enumerate(x):
         for j, element in enumerate(sample):
-            if j < size:
+            if j < size-1:
                 cost[id] += (x[id, j+1] - 1.0) * 50. * (x[id, j+1]-1.0) + lam * ux[j] * ex[id, j]
                 cost[id] += y[id, j+1] * 50. * y[id, j+1] + lam * uy[j] * ey[id, j]
-                cost[id] += vx[id, j+1] * .25 * vx[id, j+1]
-                cost[id] += vy[id, j+1] * .25 * vy[id, j+1]
+                cost[id] += vx[id, j+1] * .5 * vx[id, j+1]
+                cost[id] += vy[id, j+1] * .5 * vy[id, j+1]
             else:
                 cost[id] += (x[id, j]-1.0) * 50. * (x[id, j]-1.0)
                 cost[id] += x[id, j] * 50. * x[id, j]
-                cost[id] += x[id, j] * .25 * x[id, j]
-                cost[id] += x[id, j] * .25 * x[id, j]
+                cost[id] += x[id, j] * .5 * x[id, j]
+                cost[id] += x[id, j] * .5 * x[id, j]
 
     beta = np.min(cost)
-    print(np.max(cost))
-    print(beta)
-    print(np.abs(cost-c))
-    print(np.mean(np.abs(cost-c)))
-    print(np.abs(beta-np.min(c)))
+    #print(np.argmin(c))
+    #print(np.argmin(cost))
+    #print(np.min(c))
+    #print(beta)
+    #print(np.amax(np.abs(cost-c)))
+    #print(np.mean(np.abs(cost-c)))
+    #print(np.abs(beta-np.min(c)))
     arg = cost-beta
     exp = np.exp(-arg)
     nabla = np.sum(exp)
     weight = exp/nabla
-    u_next = np.zeros((size, 2))
-    for t in range(size):
+    u_next = np.zeros((size-1, 2))
+    u_next[:, 0] = ux_prev
+    u_next[:, 1] = uy_prev
+    for t in range(size-1):
         for i in range(samples):
             u_next[t, 0] += weight[i]*ex[i, t]
             u_next[t, 1] += weight[i]*ey[i, t]
@@ -107,8 +112,23 @@ def plot_file(file):
     #print(nabla)
     #print(weight)
 
-    print(np.abs(weight-w))
+    #print(np.abs(weight-w))
 
+    df = pd.DataFrame({"c_gpu" : c,
+                       "c_python" : cost,
+                       "c_diff" : cost - c,
+                       "w_gpu" : w,
+                       "w_python": weight,
+                       "w_diff": weight - w})
+    df.to_csv("diff_c.csv", index=False)
+
+    df = pd.DataFrame({"ux_gpu": ux,
+                       "ux_python": u_next[:, 0],
+                       "ux_diff": ux - u_next[:, 0],
+                       "uy_gpu": uy,
+                       "uy_python": u_next[:, 1],
+                       "uy_diff": uy - u_next[:, 1]})
+    df.to_csv("act_next.csv", index=False)
 
     plt.figure(0)
     circle1 = plt.Circle((1, 0), 0.01, color='r', fill=False)
@@ -118,20 +138,21 @@ def plot_file(file):
     ax.set_xlim((-1.5, 1.5))
     ax.set_ylim((-1.5, 1.5))
     ax.add_artist(circle1)
+    i = np.argmax(w)
+    j = np.argmax(weight)
+    #print(i)
+    #print(w[i])
+    #print(j)
+    #print(weight[j])
+    #ax.plot(x[j], y[j], '-b')
     for id, sample in enumerate(x):
-        if cost[id] < 2250 :
-            ax.plot(x[id], y[id], '-b')
-            print("Here: ")
-            print(id)
-            print(weight[id])
-        if w[id] > 0.01:
-            ax.plot(x[id], y[id], '-r')
-            print("This: ")
-            print(id)
-            print(weight[id])
+        #if w[id] > 0.00001:
+        ax.plot(x[id], y[id], '-b')
         #ax.quiver(x[id, 1::2], y[id, 1::2], vx[id, 1::2], vy[id, 1::2])
         #ax.yscale('linear')
         #ax.xscale('linear')
+    print(i)
+    ax.plot(x[i], y[i], '-r')
 
 
     '''
@@ -146,4 +167,4 @@ def plot_file(file):
 
 
 if __name__ == '__main__':
-    plot_file("../build/to_plot.csv")
+    plot_file("../build/to_plot.csv1")
---
+++ mppi_gpu/scripts/plot_traj.py
@@ -0,0 +1,118 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+def getMetaDataTraj(csvfile):
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        x_size = int(row['size_x'])
+        u_size = int(row['size_u'])
+        break
+
+    return x_size, u_size
+
+def getMetaDataStep(file):
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        sample = 1
+        size = 0
+        id_prev = 0
+        for row in reader:
+            if id_prev < int(row['sample']):
+                sample += 1
+                id_prev = int(row['sample'])
+            size += 1
+        size /= sample
+    return int(sample), int(size)
+
+def getBestSampleArg(file):
+    samples, size = getMetaDataStep(file)
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        arg_min = 0
+        max = -1
+        for i, row in enumerate(reader):
+            id = int(row['sample'])
+            if id*size + i < samples:
+                tmp = float(row['w'])
+                if tmp > max:
+                    max = tmp
+                    arg_max = i
+    return arg_max, samples, size
+
+def getBestSample(file):
+    id, samples, size = getBestSampleArg(file)
+    print(id)
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        best_x = np.zeros((size))
+        best_y = np.zeros((size))
+        i = 0
+        for row in reader:
+            if int(row['sample']) == id:
+                best_x[i] = float(row['x'])
+                best_y[i] = float(row['y'])
+                i += 1
+            if int(row['sample']) > id:
+                break
+    return best_x, best_y
+
+
+
+def plot_files(file):
+    x = []
+    y = []
+    vx = []
+    vy = []
+    ux = []
+    uy = []
+    x_size = 0
+    u_size = 0
+    #my_data = np.genfromtxt(file, delimiter=',')
+    #print(my_data.shape)
+
+
+    with open(file) as csvfile:
+        x_size, u_size = getMetaDataTraj(csvfile)
+        x = np.zeros((x_size))
+        y = np.zeros((x_size))
+        vx = np.zeros((x_size))
+        vy = np.zeros((x_size))
+        ux = np.zeros((u_size))
+        uy = np.zeros((u_size))
+
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for i, row in enumerate(reader):
+            if i < u_size :
+                ux[i] = float(row['ux'])
+                uy[i] = float(row['uy'])
+            x[i] = float(row['x'])
+            y[i] = float(row['y'])
+            vx[i] = float(row['vx'])
+            vy[i] = float(row['vy'])
+
+
+        #ax.yscale('linear')
+        #ax.xscale('linear')
+
+    for i in range(u_size):
+        plt.figure(1)
+        circle1 = plt.Circle((1, 0), 0.01, color='r', fill=False)
+
+        ax = plt.gca()
+        ax.cla()
+        ax.set_xlim((-1.5, 1.5))
+        ax.set_ylim((-1.5, 1.5))
+        ax.add_artist(circle1)
+        ax.plot(x, y, '-b')
+        ax.quiver(x[:i], y[:i], ux[:i], uy[:i])
+        file = "../build/to_plot.csv" + str(i)
+        best_x, best_y = getBestSample(file)
+        ax.plot(best_x, best_y, '-r')
+        plt.show()
+
+
+
+if __name__ == '__main__':
+    plot_files("../build/traj_to_plot.csv")
